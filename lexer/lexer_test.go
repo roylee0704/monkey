f6951b53dd60972579d97f694ffb8addca4391be
roylee0704 <roylee0704@gmail.com>
2018-08-12 17:14:29 +0800

    lexer: Add new test input

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,31 +7,68 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
+	input := `let five = 5;
+	let ten = 10;
+
+	let add = func(x, y) {
+		x + y;
+	};
+
+	let result = add(five, ten);
+	`
+
 	tests := []struct {
 		WantTokenType    token.Type
 		WantTokenLiteral string
 	}{
-		{token.PLUS, "+"},
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.EQUAL, "="},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.EQUAL, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.EQUAL, "="},
+		{token.FUNCTION, "func"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.EQUAL, "="},
+		{token.FUNCTION, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
 	}
-
-	input := `+=;(){}`
 
 	l := New(input)
 
 	for _, test := range tests {
 
-		itm := l.NextToken()
-		if itm.Typ != test.WantTokenType {
-			t.Errorf("invalid token type: want: '%v', got: '%v'", test.WantTokenType, itm.Typ)
+		tok := l.NextToken()
+		if tok.Typ != test.WantTokenType {
+			t.Errorf("invalid token type: want: '%v', got: '%v'", test.WantTokenType, tok.Typ)
 		}
-		if itm.Literal != test.WantTokenLiteral {
-			t.Errorf("invalid token literal: want: '%v', got: '%v'", test.WantTokenLiteral, itm.Literal)
+		if tok.Literal != test.WantTokenLiteral {
+			t.Errorf("invalid token literal: want: '%v', got: '%v'", test.WantTokenLiteral, tok.Literal)
 		}
 	}
 
